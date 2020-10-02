2a24dd91cfde43615e47f5bc594b8fcb01f9ac5e
Fan5 <openconvenience@gmail.com>
2020-10-03 07:00:43 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 const port = 3000
 
 mongoose.connect('mongodb://localhost/restaurant-list', { useNewUrlParser: true, useUnifiedTopology: true })
@@ -15,9 +16,12 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
 //set routing
 app.get('/', (req, res) => {
-  res.send('this is index')
+  res.render('index')
 })
 
 app.listen(port, () => {
