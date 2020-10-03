a3d93d2f931ebedc9ec9fe1a2bf39e2bcbb16a8f
Fan5 <openconvenience@gmail.com>
2020-10-04 02:55:26 +0800

    feat: add updat restaurant function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
 const Restaurant = require('./models/restaurant')
 const port = 3000
 
@@ -19,7 +20,7 @@ db.once('open', () => {
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
-
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
 //set routing
@@ -29,6 +30,7 @@ app.get('/', (req, res) => {
     .then((restaurants) => {
       res.render('index', { restaurants })
     })
+    .catch(error => console.log(error))
 })
 
 app.get('/restaurants/:id', (req, res) => {
@@ -36,9 +38,46 @@ app.get('/restaurants/:id', (req, res) => {
   Restaurant.findById(id)
     .lean()
     .then((restaurant) => {
-      console.log(restaurant)
       res.render('detail', { restaurant })
     })
+    .catch(error => console.log(error))
+})
+
+app.get('/restaurants/:id/edit', (req, res) => {
+  Restaurant.find()
+    .lean()
+    .then((restaurants) => {
+      //generate category list
+      let categoryList = []
+      restaurants.forEach(restaurant => categoryList.push(restaurant.category))
+      categoryList = [...new Set(categoryList)]
+
+      //find target restaurant
+      const id = req.params.id
+      const restaurant = restaurants.find(restaurant => restaurant._id.toString() === id)
+
+      //render the edit page
+      res.render('edit', { restaurant, categoryList })
+    })
+    .catch(error => console.log(error))
+})
+
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  const editedItems = req.body
+  Restaurant.findById(id)
+    .then((restaurant) => {
+      restaurant.name = editedItems.name
+      restaurant.category = editedItems.category
+      restaurant.location = editedItems.location
+      restaurant.google_map = editedItems.google_map
+      restaurant.phone = editedItems.phone
+      restaurant.description = editedItems.description
+      restaurant.image = editedItems.image
+      return restaurant.save()
+    })
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch(error => console.log(error))
 })
 
 app.listen(port, () => {
