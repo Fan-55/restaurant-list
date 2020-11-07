9fce0b303652b8605cc39f62518aae5f583715ea
Fan5 <openconvenience@gmail.com>
2020-11-08 05:07:38 +0800

    modify restaurantSeeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -1,19 +1,30 @@
 const Restaurant = require('../restaurant')
+const User = require('../user')
+const bcrypt = require('bcryptjs')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const db = require('../../config/mongoose')
 
 db.once('open', () => {
-  const restaurantSeeds = require('./restaurant.json').results
-  for (const restaurant of restaurantSeeds) {
-    Restaurant.create({
-      name: restaurant.name,
-      name_en: restaurant.name_en,
-      category: restaurant.category,
-      image: restaurant.image,
-      location: restaurant.location,
-      phone: restaurant.phone,
-      google_map: restaurant.google_map,
-      rating: restaurant.rating,
-      description: restaurant.description
+  const USER_SEED = require('./data/user.json').users[0]
+  bcrypt
+    .genSalt(10)
+    .then(salt => bcrypt.hash(USER_SEED.password, salt))
+    .then(hash => User.create({ name: USER_SEED.name, email: USER_SEED.email, password: hash }))
+    .then(user => {
+      const userId = user._id
+      const RESTAURANT_SEED = require('./data/restaurant.json').results
+      for (const restaurant of RESTAURANT_SEED) {
+        restaurant.userId = userId
+      }
+      return Restaurant.insertMany(RESTAURANT_SEED)
     })
-  }
+    .then(() => {
+      console.log('done')
+      process.exit()
+    })
+    .catch(err => console.log(err))
 })
