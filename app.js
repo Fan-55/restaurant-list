ae3000f71af6ff28b891d630ee4716ecafa0fef0
Fan5 <openconvenience@gmail.com>
2020-11-07 16:57:26 +0800

    feat: add facebook login

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const session = require('express-session')
 const passport = require('passport')
+const flash = require('connect-flash')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -31,9 +32,13 @@ app.use(session({
 require('./config/passport')
 app.use(passport.initialize())
 app.use(passport.session())
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  res.locals.error = req.flash('error')
   next()
 })
 app.use(routes)
--- routes/index.js
+++ routes/index.js
@@ -6,10 +6,12 @@ const restaurants = require('./modules/restaurants')
 const search = require('./modules/search')
 const filter = require('./modules/filter')
 const users = require('./modules/users')
+const auth = require('./modules/auth')
 
 const { authenticator } = require('../middleware/auth')
 
 router.use('/users', users)
+router.use('/auth', auth)
 router.use('/restaurants', authenticator, restaurants)
 router.use('/search', authenticator, search)
 router.use('/filter', authenticator, filter)
